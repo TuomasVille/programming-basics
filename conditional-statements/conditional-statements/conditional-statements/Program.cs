46cc6d5822993c7fd1112e72c37c792d6c16eade
TuomasVille <tuomas_kayhty@hotmail.com>
2017-09-22 13:06:28 +0300

    Initial commit

---
+++ conditional-statements/conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define instructions
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            bool isNumber;
+            do
+            {
+
+                Console.Write("Syötä luku: ");
+                // define variables
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int evaluatedNumber;
+                isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+                // programming logics
+                if (isNumber == true)
+                {
+                    if (evaluatedNumber < 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+                    }
+                    else if (evaluatedNumber > 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syötit muuta kuin numeron");
+                }
+            } while (isNumber == true);
+            Console.ReadKey();
+           
+        }
+    }
+}
