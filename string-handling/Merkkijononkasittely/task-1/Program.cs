f38ac8fe3b2837e2f559a9c39f95e6486f8fc2aa
TuomasVille <tuomas_kayhty@hotmail.com>
2017-12-16 16:51:31 +0200

    Final

---
+++ string-handling/Merkkijononkasittely/task-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma ilmoittaa syötteestä merkkien lukumäärän:");
+            Console.WriteLine("Kirjoita jotain:");
+            
+            while(true)
+            {
+                string userInput = Console.ReadLine();
+                Console.WriteLine($"Syötteessä on {userInput.Length} merkkiä.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ string-handling/Merkkijononkasittely/task-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma muuttaa käyttäjän syöttämästä syötteestä e kirjaimet @ merkiksi:");
+            Console.WriteLine("Kirjoita jotain:");
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                Console.WriteLine(userInput.Replace("e", "@"));
+                Console.ReadKey();              
+            }
+        }
+    }
+}
